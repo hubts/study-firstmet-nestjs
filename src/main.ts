4f0e1f21a7e047500737dae2df9836db12827720
kimts <kimts.ibct@gmail.com>
2021-04-26 18:59:45 +0900

    UNIT TESTING

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Validation Check
+      forbidNonWhitelisted: true, // Force Validation Check (forbid request)
+      transform: true // automatically, 'type' is transformed for Entity(Dto)
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
