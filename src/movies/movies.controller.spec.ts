f04a49ffe62078ca93c9e7a5b713594b7ed81bec
kimts <kimts.ibct@gmail.com>
2021-04-27 14:17:47 +0900

    LEARN UNIT TEST

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Movie } from './entities/movie.entity';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
+import * as faker from 'faker';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 describe('MoviesController', () => {
   let controller: MoviesController;
@@ -18,5 +21,61 @@ describe('MoviesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array of movies', async () => {
+      const result = [];
+      jest.spyOn(service, 'getAll').mockImplementation(() => result);
+      expect(await controller.getAll()).toBe(result);
+      // controller.getAll();
+      // expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', async () => {
+      const testMovie: CreateMovieDto = {
+        title: faker.name.title(),
+        genres: [faker.music.genre()],
+        year: faker.datatype.number({min:2000, max:2020})
+      };
+      service.create(testMovie);
+      
+      const savedMovie: Movie = {
+        id: 1,
+        ...testMovie
+      };
+      const movieGetOneSpy = jest.spyOn(service, 'getOne').mockReturnValue(savedMovie);
+      const result = controller.getOne(1);
+      
+      expect(movieGetOneSpy).toBeCalledWith(1);
+      expect(result).toEqual(savedMovie);
+      expect(result.title).toEqual(savedMovie.title);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const testMovieDto : CreateMovieDto = {
+        title: faker.name.title(),
+        genres: [faker.music.genre()],
+        year: faker.datatype.number({min:2000, max:2020})
+      }
+      const expectedMovie: Movie = {
+        id: 1,
+        ...testMovieDto
+      };
+      const movieGetOneSpy = jest.spyOn(service, 'getOne').mockReturnValue(expectedMovie);
+
+      const beforeCreateMovie = service.getAll().length;
+      controller.create(testMovieDto);
+      const afterCreateMovie = service.getAll().length;
+      const result = controller.getOne(1);
+
+      expect(movieGetOneSpy).toBeCalledWith(1);
+      expect(result).toEqual(expectedMovie);
+      expect(afterCreateMovie).toBeGreaterThan(beforeCreateMovie);      
+    });
   });
 });
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Logger, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
@@ -6,10 +6,13 @@ import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  private readonly logger = new Logger(MoviesController.name);
+
   constructor(private readonly moviesService: MoviesService) {}
 
   @Get()
   getAll(): Movie[] {
+    // this.logger.log('Call getAll()');
     return this.moviesService.getAll();
   }
   
